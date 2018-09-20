8088df91b6b443cc8edeeee692f46c0346f1f7e5
Xiaoping Li <narci2008@gmail.com>
2018-09-20 16:17:59 -0700

    Add example

--- Resolutions/3Sum.js
+++ Resolutions/3Sum.js
@@ -2,3 +2,11 @@
 
 // Note:
 // The solution set must not contain duplicate triplets.
+
+// Example:
+// Given array nums = [-1, 0, 1, 2, -1, -4],
+// A solution set is:
+// [
+//   [-1, 0, 1],
+//   [-1, -1, 2]
+// ]
