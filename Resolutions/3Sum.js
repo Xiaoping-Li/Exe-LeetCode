cdebf449c03d80d66a850b4328cb33466affb3ee
Xiaoping Li <narci2008@gmail.com>
2018-09-20 16:19:36 -0700

    Add solution

--- Resolutions/3Sum.js
+++ Resolutions/3Sum.js
@@ -10,3 +10,19 @@
 //   [-1, 0, 1],
 //   [-1, -1, 2]
 // ]
+
+// Timeout solution: This solution is pretty bad, Time Complexity Big O: n^4
+var threeSum = function(nums) {
+  const result = [];
+  for (let i = 0; i < nums.length; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+          for (let m = j + 1; m < nums.length; m++) {
+            if (nums[i] + nums[j] + nums[m] === 0) {
+              let tripletStr = [nums[i], nums[j], nums[m]].sort((a,b) => a - b).join(' ');
+              if (!result.includes(tripletStr)) result.push(tripletStr);
+            }
+          }
+      }
+  }
+  return result.map(item => item.split(' ')).map(ele => ele.map(str => parseInt(str)));
+};
