dacabea8345f85a2d1066361b3f7de580be29771
Xiaoping-Li <narci2008@gmail.com>
2018-10-11 13:44:36 -0400

    Update 3Sum.js

--- Resolutions/3Sum.js
+++ Resolutions/3Sum.js
@@ -11,6 +11,40 @@
 //   [-1, -1, 2]
 // ]
 
+
+// Beat 90% solution: reference https://leetcode.com/problems/3sum/discuss/7407/JavaScript-Beat-94.29
+const threeSum = function(nums) {
+  const result = [];
+  if (nums.length < 3) return result;
+
+  nums.sort((a,b) => a - b);
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    for (let j = i + 1, k = nums.length - 1; j < k;) {
+      if (nums[i] + nums[j] + nums[k] === 0) {
+        result.push([nums[i], nums[j], nums[k]]);
+        j++;
+        k--;
+        while (j < k && nums[j] === nums[j - 1]) {
+          j++;
+        }
+        while (j < k && nums[k] === nums[k + 1]) {
+          k--;
+        }
+      } else if (nums[i] + nums[j] + nums[k] > 0) {
+        k--;
+      } else {
+        j++;
+      }
+    }
+  }
+
+  return result;
+};
+
+
+
+
 // Beat 1.9% Solution: Pretty bad, need improve. Use {} to store combinations 
 
 const threeSum = function(nums) {
@@ -151,4 +185,3 @@ const threeSum = function(nums) {
 
   return result;
 };
-
