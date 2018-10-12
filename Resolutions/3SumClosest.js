9e7755d809617a9f6a5b2109cb2dadd1b4bcaa45
Xiaoping-Li <narci2008@gmail.com>
2018-10-12 11:00:23 -0400

    Update 3SumClosest.js

--- Resolutions/3SumClosest.js
+++ Resolutions/3SumClosest.js
@@ -4,3 +4,42 @@
 // Example:
 // Given array nums = [-1, 2, 1, -4], and target = 1.
 // The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+// Beat 3.35% Solution:
+const threeSumClosest = function(nums, target) {
+  nums.sort((a,b) => a - b);
+  let n = nums.length;
+  let closest = nums[n - 3] + nums[n - 2] + nums[n - 1];
+  let diff = Math.abs(closest - target);
+  if (diff === 0) return closest;
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i - 1] === nums[i] && nums[i] === nums[i + 1] && nums[i] === nums[i + 2]) continue;
+    for (let j = i + 1, k = nums.length - 1;j < k;) {
+      let temp = nums[i] + nums[j] + nums[k];
+
+      if (Math.abs(temp - target) < diff) {
+        closest = temp;
+        diff = Math.abs(temp - target);
+        k--;
+        while (j < k && nums[k] === nums[k + 1]) {
+          k--;
+        }
+        if (j === k) {
+          j++;
+          k = nums.length - 1
+        }
+      } else {
+        k--;
+        while (j < k && nums[k] === nums[k + 1]) {
+          k--;
+        }
+        if (j === k) {
+          j++;
+          k = nums.length - 1
+        }
+      }  
+    }
+  }
+  return closest;
+};
