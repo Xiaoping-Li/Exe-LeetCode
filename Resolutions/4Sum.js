7d2a9aabf965b8b5252403108c85b9c05b4fe57f
Xiaoping-Li <narci2008@gmail.com>
2018-10-15 10:33:19 -0400

    Update 4Sum.js

--- Resolutions/4Sum.js
+++ Resolutions/4Sum.js
@@ -13,3 +13,46 @@
   [-2,  0, 0, 2]
 ]
 */
+
+// Solution:
+const fourSum = function(nums, target) {
+  let rtn = [];
+  if (nums.length < 4) return rtn;
+  nums.sort((a,b) => a - b);
+
+  for (let i = 0; i < nums.length - 3; i++) {
+      if (i > 0 && nums[i - 1] === nums[i] && nums[i] === nums[i + 1] && nums[i] === nums[i + 2]) continue;
+    // console.log(i)
+    for (let j = i + 1, k = j + 1, l = nums.length - 1; k < l;) {
+      console.log(nums[k], nums[l]);
+      let temp = nums[i] + nums[j] + nums[k] + nums[l];
+      if (temp === 0) {
+        rtn.push([nums[i], nums[j], nums[k], nums[l]]);
+        k++;
+        l--;
+
+
+      } else if (temp > 0) {
+        l--;
+        while(nums[l] === nums[l + 1]) {
+          l--;
+        }
+      } else {
+        k++;
+        while(nums[k] === nums[k - 1]) {
+          k++;
+        }
+      }
+
+      // if (k === l) {
+      //   j++;
+      //   k = j + 1;
+      //   l = nums.length - 1;
+      // } 
+    }
+  } 
+
+  return rtn; 
+};
+
+fourSum([1, 0, -1, 0, -2, 2, ]);
