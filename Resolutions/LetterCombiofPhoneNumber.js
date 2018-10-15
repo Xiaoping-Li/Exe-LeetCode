7a115b8781a273b5b3c2aedfdb2c339a9f9ca453
Xiaoping-Li <narci2008@gmail.com>
2018-10-15 11:59:12 -0400

    Update LetterCombiofPhoneNumber.js

--- Resolutions/LetterCombiofPhoneNumber.js
+++ Resolutions/LetterCombiofPhoneNumber.js
@@ -4,3 +4,34 @@
 // Example:
 // Input: "23"
 // Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
+
+// Beat 94% solution:
+const letterCombinations = function(digits) {
+  const obj = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
+  };
+
+  const array = digits.split('');
+  if (array.length === 0) return [];
+  if (array.length < 2) return obj[array[0]].split('');
+  const firstDigit = array.shift();
+  let rtn = obj[firstDigit].split('');
+  
+  while (array.length > 0) {
+    let nextDigit = array.shift();
+    let next = obj[nextDigit].split('');
+    rtn = rtn.map(item => {
+      return next.map(ele => item + ele);
+    });
+    rtn = rtn.reduce((a,b) => a.concat(b))
+  }
+  return rtn;
+};
