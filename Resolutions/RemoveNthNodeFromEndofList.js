9f12d8ba406c35d35383b0789a5b61fae68cc8e9
Xiaoping-Li <narci2008@gmail.com>
2018-10-15 16:54:43 -0400

    Update RemoveNthNodeFromEndofList.js

--- Resolutions/RemoveNthNodeFromEndofList.js
+++ Resolutions/RemoveNthNodeFromEndofList.js
@@ -5,3 +5,45 @@
 // After removing the second node from the end, the linked list becomes 1->2->3->5.
 // Note:
 // Given n will always be valid.
+
+// Beat 100% solution:
+var removeNthFromEnd = function(head, n) {
+  // Count the length of the linked list: head
+  let length = 1;
+  let current = head;
+  while (current.next) {
+    current = current.next;
+    length++;
+  }
+  // If only one node, after remove, will return null;
+  if (length === 1) return null;
+  
+  // More than one node:
+  // Define i: count node from head
+  let i = length - n;
+  let preNth = head;
+  // If i === 0, remove current headNode, return new head node
+  if (i === 0) {
+    head = head.next;
+    return head;
+  } else if (i === 1) {
+    // If remove the node next to head
+    preNth = head; 
+  } else {
+    while (i > 1) {
+      preNth = preNth.next;
+      i--;
+    }
+  }
+  
+  let nextNth = preNth.next.next;
+  // If remove the last node:
+  if (!nextNth) {
+    preNth.next = null;
+  } else {
+    // Remove node in front of last node:
+    preNth.next = nextNth;
+  }
+  
+  return head;  
+};
