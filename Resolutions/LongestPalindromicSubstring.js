62598e11777c29b5192cdc1227ee2c903b716ca2
Xiaoping-Li <narci2008@gmail.com>
2018-10-18 11:36:28 -0400

    Update LongestPalindromicSubstring.js

--- Resolutions/LongestPalindromicSubstring.js
+++ Resolutions/LongestPalindromicSubstring.js
@@ -10,4 +10,27 @@
 // Output: "bb"
 
 // Beat 11% Solution:
+const longestPalindrome = function(s) {
+  if (s.length < 2) return s;
+  let longest = s[0]; 
+  for (let i = 0; i < s.length; i++) {
+    if (s.length - i <= longest.length) return longest;
+    for (let j = s.length - 1; j > i; j--) {
+      if (s[i] === s[j]) {
+        let m = i;
+        let n = j;
+        while (s[m] === s[n] && m < n) {
+          m++;
+          n--;
+        }
+        if (n - m < 1 ) {
+          let palind = s.slice(i, j+1);
+          if (palind.length > longest.length) longest = palind;
+        }
+      }
+    }
+  }
+  return longest;    
+};
 
+longestPalindrome('aaaabaaa');
