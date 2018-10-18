49430783390d1e300c50fd77c1ea35f873a79186
Xiaoping-Li <narci2008@gmail.com>
2018-10-18 11:35:49 -0400

    Update LongestPalindromicSubstring.js

--- Resolutions/LongestPalindromicSubstring.js
+++ Resolutions/LongestPalindromicSubstring.js
@@ -10,25 +10,4 @@
 // Output: "bb"
 
 // Output limit exceeded solution:
-var longestPalindrome = function(s) {
-  let longest = s[0];
-  if (s.length < 2) return s;
-  for (let i = 0; i < s.length; i++) {
-    for (let j = s.length - 1; j > i; j--) {
-      if (s[i] === s[j]) {
-        let m = i;
-        let n = j;
-        while (s[m] === s[n] && m < n) {
-          m++;
-          n--;
-        }
-        if (n - m < 1 ) {
-          let palind = s.slice(i, j+1);
-          if (palind.length > longest.length) longest = palind;
-          console.log(palind);
-        }
-      }
-    }
-  }
-  return longest;    
-};
+
