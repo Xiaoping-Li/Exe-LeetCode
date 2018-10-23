432e53e1107469c596a642a9bb95049a3da1293e
Xiaoping-Li <narci2008@gmail.com>
2018-10-23 16:20:07 -0700

    Update 0003-LongestSubstringWithoutRepeatingCharacters.js

--- Resolutions/0003-LongestSubstringWithoutRepeatingCharacters.js
+++ Resolutions/0003-LongestSubstringWithoutRepeatingCharacters.js
@@ -19,4 +19,25 @@ Explanation: The answer is "wke", with the length of 3.
              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 */
 
-// Beats 3% Solution: 
+// Beats 3% Solution: Time Complexity O(n^2)
+var lengthOfLongestSubstring = function(s) {
+  let longest = 0;
+  for (let i = 0; i < s.length;i++) {
+    if (s[i] === s[i + 1]) continue;
+    if (s.length - i <= longest) return longest;
+    const obj = {};
+    for (let j = i; j < s.length; j++) {
+      if (!obj[s[j]]) {
+        obj[s[j]] = 1;
+      } else {
+        break;
+      }
+    }
+
+    let subLength = Object.keys(obj).length;
+    if (subLength > longest) {
+      longest = subLength;
+    }     
+  } 
+  return longest;
+};
