223ead8052ce3620af9509d603d120bdc37cea7f
Xiaoping-Li <narci2008@gmail.com>
2018-10-23 14:09:39 -0700

    Update 0001-TwoSum.js

--- Resolutions/0001-TwoSum.js
+++ Resolutions/0001-TwoSum.js
@@ -11,6 +11,20 @@ Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 */
 
+
+// Beats 32% Solution:
+var twoSum = function(nums, target) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+
 // Beats 13% Solution:
 var twoSum = function(nums, target) {
   let result = [];
