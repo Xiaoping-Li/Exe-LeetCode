c340053867a85961f87b9593ff3f6e906528867c
Xiaoping-Li <narci2008@gmail.com>
2018-10-24 13:36:18 -0700

    Update 0007-ReverseInteger.js

--- Resolutions/0007-ReverseInteger.js
+++ Resolutions/0007-ReverseInteger.js
@@ -17,3 +17,10 @@ Note:
 Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the
 purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 */
+
+// Beats 5% Solution:
+var reverse = function(x) {
+    let reverseX = Number(Math.abs(x).toString().split('').reverse().join(''));
+    x < 0 ? reverseX *= (-1) : reverseX;
+    return reverseX < -1 * Math.pow(2, 31) || reverseX > (Math.pow(2, 31) - 1) ? 0 : reverseX;   
+};
