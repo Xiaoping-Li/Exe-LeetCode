a5f938b8dd19f9dad8f686765262ee8da0895400
Xiaoping-Li <narci2008@gmail.com>
2018-10-24 13:51:19 -0700

    Update 0007-ReverseInteger.js

--- Resolutions/0007-ReverseInteger.js
+++ Resolutions/0007-ReverseInteger.js
@@ -18,6 +18,23 @@ Assume we are dealing with an environment which could only store integers within
 purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 */
 
+
+// Beats 65% Solution:
+var reverse = function(x) {
+  const xStr = x.toString();
+  let reverse = '';
+  for (let i = xStr.length - 1; i > -1; i--) {
+    if (parseInt(xStr[i])) {
+      reverse += xStr[i];
+    }
+  } 
+
+  let result;
+  x >= 0 ? result = parseInt(reverse) : result = parseInt(-1 * reverse); 
+  return result >= -1 * Math.pow(2, 31) && result <= Math.pow(2, 31) - 1 ? result : 0; 
+};
+
+
 // Beats 5% Solution:
 var reverse = function(x) {
     let reverseX = Number(Math.abs(x).toString().split('').reverse().join(''));
