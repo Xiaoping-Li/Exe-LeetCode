1e52b9dbfd06b55a4271ef9e0a59b6499f227735
Xiaoping-Li <narci2008@gmail.com>
2018-10-25 15:18:23 -0700

    Update 0021-MergeTwoSortedLists.js

--- Resolutions/0021-MergeTwoSortedLists.js
+++ Resolutions/0021-MergeTwoSortedLists.js
@@ -7,3 +7,17 @@ Example:
 Input: 1->2->4, 1->3->4
 Output: 1->1->2->3->4->4
 */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
