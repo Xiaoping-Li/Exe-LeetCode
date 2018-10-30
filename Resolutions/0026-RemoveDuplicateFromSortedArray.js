e9d8a0f3dd37d858cd828009da84370c23581e80
Xiaoping-Li <narci2008@gmail.com>
2018-10-30 14:53:48 -0700

    Update 0026-RemoveDuplicateFromSortedArray.js

--- Resolutions/0026-RemoveDuplicateFromSortedArray.js
+++ Resolutions/0026-RemoveDuplicateFromSortedArray.js
@@ -9,6 +9,27 @@
 // Input: Given nums = [0,0,1,1,1,2,2,3,3,4],
 // Output: Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively. It doesn't matter what values are set beyond the returned length.
 
+
+// Time complexity: O(n)
+var removeDuplicates = function(nums) {
+  let size = nums.length;
+  let start = 1;
+  for (let i = start; i < nums.length; i++) {
+    let current = nums[i];
+    let preStart = nums[start - 1];
+    if (current !== preStart) {
+      nums[start] = nums[i];
+      start++;
+    } else {
+      size--;
+    }
+  }
+  return size;
+};
+
+
+
+
 // Beats 42% Solution: O(n^2)
 const removeDuplicates = function(nums) {
   if (nums.length === 0) return 0;
