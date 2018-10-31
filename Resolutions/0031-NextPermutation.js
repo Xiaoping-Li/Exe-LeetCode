24639897aa94937943ea612033a607fb1229d5c8
Xiaoping-Li <narci2008@gmail.com>
2018-10-31 15:37:29 -0700

    Update 0031-NextPermutation.js

--- Resolutions/0031-NextPermutation.js
+++ Resolutions/0031-NextPermutation.js
@@ -14,10 +14,10 @@ Here are some examples. Inputs are in the left-hand column and its corresponding
 */
 
 
-// Beats 99% solution:
+// Beats 99% solution: 
 var nextPermutation = function(nums) {
   if (nums.length <= 1) return nums;
-
+  // Helper function downSort for decsent nums: Big O(n)
   const downSort = (nums, n) => {
     for (let i = n, j = nums.length - 1; i < j; i++, j--) {
       let iVal = nums[i];
@@ -26,7 +26,7 @@ var nextPermutation = function(nums) {
       nums[j] = iVal;
     }
   }
-
+  // Helper function bubbleSort: Big O (n ^ n)
   const bubbleSort = (nums, n) => {
     let count = nums.length - n;
     while(count > 0) {
@@ -41,16 +41,18 @@ var nextPermutation = function(nums) {
       count--;
     }
   }
-
+  // descent is a flag
+  // sortIdx will be used in bubble sort
   let descent = true;
   let sortIdx;
   for (let i = nums.length - 1; i > 0; i--) {
     let current = nums[i];
     let prev = nums[i - 1];
+    // outloop is a flag to break out layer for loop
     let outloop = false;
     if (current > prev) {
       descent = false;
-
+      // from i to the end, find the smallest val that > prev && < current, record val, and it's index
       let smallest = [current, i];
       for (let j = i; j < nums.length; j++) {
         if (nums[j] > prev && nums[j] < current) {
@@ -58,17 +60,19 @@ var nextPermutation = function(nums) {
           smallest[1] = j;
         }
       }
+      // Swap smallest with prev
       nums[i - 1] = smallest[0];
       nums[smallest[1]] = prev;
+      // Find sortIex and set outloop breaker to true 
       sortIdx = i;
       outloop = true;
       break;
     }
     if (outloop) break;
   }
-
+  // Bubble sort nums from sortIdx (i)
   bubbleSort(nums, sortIdx);
-
+  // If nums is sorted in descent order, use downsort directly. 
   if (descent) downSort(nums, 0); 
   return nums;  
 };
