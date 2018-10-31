cd81854b0e8c07e1cd47d88e3b2793d832d5876f
Xiaoping-Li <narci2008@gmail.com>
2018-10-31 15:30:36 -0700

    Update 0031-NextPermutation.js

--- Resolutions/0031-NextPermutation.js
+++ Resolutions/0031-NextPermutation.js
@@ -12,3 +12,63 @@ Here are some examples. Inputs are in the left-hand column and its corresponding
 1,1,5 → 1,5,1
 1,3,2 → 2,1,3 
 */
+
+
+// Beats 99% solution:
+var nextPermutation = function(nums) {
+  if (nums.length <= 1) return nums;
+
+  const downSort = (nums, n) => {
+    for (let i = n, j = nums.length - 1; i < j; i++, j--) {
+      let iVal = nums[i];
+      let jVal = nums[j];
+      nums[i] = jVal;
+      nums[j] = iVal;
+    }
+  }
+
+  const bubbleSort = (nums, n) => {
+    let count = nums.length - n;
+    while(count > 0) {
+      for (let i = n; i < nums.length; i++) {
+        let current = nums[i];
+        let next = nums[i + 1];
+        if (current > next) {
+          nums[i] = next;
+          nums[i + 1] = current;
+        }
+      }
+      count--;
+    }
+  }
+
+  let descent = true;
+  let sortIdx;
+  for (let i = nums.length - 1; i > 0; i--) {
+    let current = nums[i];
+    let prev = nums[i - 1];
+    let outloop = false;
+    if (current > prev) {
+      descent = false;
+
+      let smallest = [current, i];
+      for (let j = i; j < nums.length; j++) {
+        if (nums[j] > prev && nums[j] < current) {
+          smallest[0] = nums[j];
+          smallest[1] = j;
+        }
+      }
+      nums[i - 1] = smallest[0];
+      nums[smallest[1]] = prev;
+      sortIdx = i;
+      outloop = true;
+      break;
+    }
+    if (outloop) break;
+  }
+
+  bubbleSort(nums, sortIdx);
+
+  if (descent) downSort(nums, 0); 
+  return nums;  
+};
