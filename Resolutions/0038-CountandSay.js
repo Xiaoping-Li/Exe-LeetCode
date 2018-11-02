928456d310db2d8bd7b273a3044ac33ec1ad6860
Xiaoping-Li <narci2008@gmail.com>
2018-11-02 09:35:47 -0700

    Update 0038-CountandSay.js

--- Resolutions/0038-CountandSay.js
+++ Resolutions/0038-CountandSay.js
@@ -1,11 +1,17 @@
 /*
 The count-and-say sequence is the sequence of integers with the first five terms as following:
 
-1.     1
-2.     11
-3.     21
-4.     1211
-5.     111221
+ 1.     1
+ 2.     11
+ 3.     21
+ 4.     1211
+ 5.     111221 
+ 6.     312211
+ 7.     13112221
+ 8.     1113213211
+ 9.     31131211131221
+10.     13211311123113112211
+
 1 is read off as "one 1" or 11.
 11 is read off as "two 1s" or 21.
 21 is read off as "one 2, then one 1" or 1211.
